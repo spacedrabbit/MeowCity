ce5531da6d7ada98289e19083d81233d775cb592
richmcateer <rmcateer1@gmail.com>
2014-11-25 14:33:10 -0500

    Tried segue. Not working. Computer soooo slow

---
+++ BarkCity/BRKVenueDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  BRKVenueDetailViewController.h
+//  BarkCity
+//
+//  Created by Richard McAteer on 11/25/14.
+//  Copyright (c) 2014 com.rosamcgee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BRKVenueDetailViewController : UIViewController
+
+@end
