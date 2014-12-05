73cac9dddfd895341a32f4a4ce8170773e7fd229
rosamcgee <rosajmcgee@gmail.com>
2014-12-05 14:12:10 -0500

    Search bar container

---
+++ BarkCity/BRKSearchBarContainer.m
@@ -0,0 +1,21 @@
+//
+//  BRKSearchBarContainer.m
+//  BarkCity
+//
+//  Created by Rosa McGee on 12/5/14.
+//  Copyright (c) 2014 com.rosamcgee. All rights reserved.
+//
+
+#import "BRKSearchBarContainer.h"
+
+@implementation BRKSearchBarContainer
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
