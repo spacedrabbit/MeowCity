c73a0bcc522e8c02d75846795a6cc92bebe22e32
rosamcgee <rosajmcgee@gmail.com>
2014-12-05 14:12:10 -0500

    Search bar container

---
+++ BarkCity/BRKSearchBarContainer.h
@@ -0,0 +1,13 @@
+//
+//  BRKSearchBarContainer.h
+//  BarkCity
+//
+//  Created by Rosa McGee on 12/5/14.
+//  Copyright (c) 2014 com.rosamcgee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BRKSearchBarContainer : UIView
+
+@end
