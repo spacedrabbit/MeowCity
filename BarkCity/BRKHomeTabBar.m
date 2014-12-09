d3263214f43701a46be721213169e8294442f66d
Louis Tur <louis.tur@gmail.com>
2014-12-08 20:32:50 -0500

    Adds an animated tab indicator to project.

--- BarkCity/BRKHomeTabBar.m
+++ BarkCity/BRKHomeTabBar.m
@@ -7,11 +7,23 @@
 //
 
 #import "BRKHomeTabBar.h"
+#import "BRKUIManager.h"
 @interface BRKHomeTabBar()
 
-@property (strong, nonatomic) NSArray * iconImagesArray;
-@property (strong, nonatomic) NSArray * iconHighlightedArray;
-@property (nonatomic) NSInteger lastSelectedTab;
+enum tabLocations   {
+    snackTab = 0,
+    cafeTab,
+    barTab,
+    shoppingTab,
+    playTab
+};
+
+@property (nonatomic)   NSInteger    lastSelectedTab     ;
+
+@property (strong,  nonatomic) NSArray  *   iconImagesArray     ;
+@property (strong,  nonatomic) NSArray  *   iconHighlightedArray;
+
+@property (strong,  nonatomic) UIImageView  * tabIndicator;
 
 @end
 
@@ -20,19 +32,19 @@
 -(void)awakeFromNib{
     [super awakeFromNib];
     
-    CGRect screenWidth = [UIScreen mainScreen].bounds;
-    CGFloat buttonWidth = screenWidth.size.width / 5.0;
-    CGFloat buttonHeight = 44.0;
+    CGRect  screenWidth     = [UIScreen mainScreen].bounds;
+    CGFloat buttonWidth     = screenWidth.size.width / 5.0;
+    CGFloat buttonHeight    = 44.0;
     
     self.tabBarView = [[UIView alloc] init];
     [self addSubview:self.tabBarView];
     
-    [self.tabBarView setTranslatesAutoresizingMaskIntoConstraints:NO];
-    [self.firstTabButton setTranslatesAutoresizingMaskIntoConstraints:NO];
-    [self.secondTabButton setTranslatesAutoresizingMaskIntoConstraints:NO];
-    [self.thirdTabButton setTranslatesAutoresizingMaskIntoConstraints:NO];
-    [self.firstTabButton setTranslatesAutoresizingMaskIntoConstraints:NO];
-    [self.fifthTabButton setTranslatesAutoresizingMaskIntoConstraints:NO];
+    [self.tabBarView        setTranslatesAutoresizingMaskIntoConstraints:NO];
+    [self.firstTabButton    setTranslatesAutoresizingMaskIntoConstraints:NO];
+    [self.secondTabButton   setTranslatesAutoresizingMaskIntoConstraints:NO];
+    [self.thirdTabButton    setTranslatesAutoresizingMaskIntoConstraints:NO];
+    [self.firstTabButton    setTranslatesAutoresizingMaskIntoConstraints:NO];
+    [self.fifthTabButton    setTranslatesAutoresizingMaskIntoConstraints:NO];
     
     NSArray * tabViewHorizontal = [NSLayoutConstraint constraintsWithVisualFormat:@"H:|[_tabBarView]|"
                                                                           options:0
@@ -45,11 +57,11 @@
     [self addConstraints:tabViewHorizontal];
     [self addConstraints:tabViewVertical];
     
-    [self.tabBarView addSubview:self.firstTabButton];
-    [self.tabBarView addSubview:self.secondTabButton];
-    [self.tabBarView addSubview:self.thirdTabButton];
-    [self.tabBarView addSubview:self.fourthTabButton];
-    [self.tabBarView addSubview:self.fifthTabButton];
+    [self.tabBarView    addSubview: self.firstTabButton ];
+    [self.tabBarView    addSubview: self.secondTabButton];
+    [self.tabBarView    addSubview: self.thirdTabButton ];
+    [self.tabBarView    addSubview: self.fourthTabButton];
+    [self.tabBarView    addSubview: self.fifthTabButton ];
     
     NSDictionary * buttonsDictionary = NSDictionaryOfVariableBindings(_firstTabButton, _secondTabButton, _thirdTabButton, _fourthTabButton, _fifthTabButton);
     
@@ -115,12 +127,26 @@
                 forControlEvents:UIControlEventTouchUpInside];
     }];
     
+    self.tabIndicator = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"select"]];
+    [self.tabIndicator setAlpha:1.0];
+    [self.tabIndicator.layer setDrawsAsynchronously:YES];
+    [self.tabIndicator.layer setShadowColor:[UIColor whiteColor].CGColor];
+    [self.tabIndicator.layer setShadowOpacity:.80];
+    [self.tabIndicator.layer setShadowRadius:6.0];
+    [self.tabIndicator.layer setShadowOffset:CGSizeMake(-1, -6)];
+    
+    
+    
+}
+-(void)layoutSubviews{
+    [self.tabBarView insertSubview:self.tabIndicator atIndex:0];
+    [self updateTabIndicatorTo:999 animated:NO];
 }
 
 -(id)initWithCoder:(NSCoder *)aDecoder{
     self = [super initWithCoder:aDecoder];
     if (self) {
-        NSLog(@"Init from coder");
+        
     }
     return self;
 }
@@ -138,12 +164,48 @@
     NSInteger indexOfButton = senderButton.tag;
     
     self.lastSelectedTab = indexOfButton;
+    [self updateTabIndicatorTo:indexOfButton animated:YES];
     
     [self.delegate didSelectTabButton:indexOfButton];
     
 }
 -(NSInteger) currentlySelectedTab{
     return self.lastSelectedTab;
+}
+-(void) updateTabIndicatorTo:(NSInteger)tabIndex animated:(BOOL)animated
+{
+    CGPoint newCenterCoordinate;
+    switch (tabIndex) {
+        case 0:
+            newCenterCoordinate = self.firstTabButton.center;
+            break;
+        case 1:
+            newCenterCoordinate = self.secondTabButton.center;
+            break;
+        case 2:
+            newCenterCoordinate = self.thirdTabButton.center;
+            break;
+        case 3:
+            newCenterCoordinate = self.fourthTabButton.center;
+            break;
+        case 4:
+            newCenterCoordinate = self.fifthTabButton.center;
+            break;
+        default:
+            newCenterCoordinate = self.firstTabButton.center;
+            break;
+    }
+    
+    if (animated)
+    {
+        [UIView animateWithDuration:.25 animations:^{
+            [self.tabIndicator setCenter:CGPointMake(newCenterCoordinate.x, 44.0)];
+        }];
+    }else
+    {
+        [self.tabIndicator setCenter:CGPointMake(newCenterCoordinate.x, 40.0)];
+    }
+    
 }
 
 @end
